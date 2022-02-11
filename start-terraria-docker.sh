4e617469fc1864d2ca2fd8aa4abf833e8a72c929
LtFubuki <99317076+LtFubuki@users.noreply.github.com>
2022-02-10 22:03:41 -0800

    Update start-terraria-docker.sh

--- start-terraria-docker.sh
+++ start-terraria-docker.sh
@@ -3,8 +3,11 @@
 GH_USER=LtFubuki
 GH_REPO=Terraria-Docker-arm64v8
 GH_BRANCH=main
+SERVER_COUNT=docker ps | grep ltfubuki-arm64v8 | wc -l
 curl -L https://github.com/${GH_USER}/${GH_REPO}/archive/refs/heads/${GH_BRANCH}.zip > ${GH_REPO}-${GH_BRANCH}.zip && \
 unzip ./${GH_REPO}-${GH_BRANCH}.zip && \
 rm ./${GH_REPO}-${GH_BRANCH}.zip && \
 cd ${GH_REPO}-${GH_BRANCH} && \
-docker build -t ltfubuki/terraria-arm64v8 .
+docker build -t ltfubuki/terraria-arm64v8 . && \
+mkdir worlds && \
+docker run -itd -v worlds:/terraria/worlds -v configs:/terraria/configs -p 7777:7777/tcp --name terraria-${SERVER_COUNT} ltfubuki/terraria-arm64v8
