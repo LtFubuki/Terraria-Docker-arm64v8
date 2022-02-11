f210e16aedc284021bc620f1b0182a54468d5f1c
LtFubuki <99317076+LtFubuki@users.noreply.github.com>
2022-02-10 21:41:05 -0800

    Update start-terraria-docker.sh

--- start-terraria-docker.sh
+++ start-terraria-docker.sh
@@ -5,4 +5,6 @@ GH_REPO=Terraria-Docker-arm64v8
 GH_BRANCH=main
 curl -L https://github.com/${GH_USER}/${GH_REPO}/archive/refs/heads/${GH_BRANCH}.zip > ${GH_REPO}-${GH_BRANCH}.zip && \
 unzip ./${GH_REPO}-${GH_BRANCH}.zip && \
-rm ./${GH_REPO}-${GH_BRANCH}.zip
+rm ./${GH_REPO}-${GH_BRANCH}.zip && \
+cd ${GH_REPO}-${GH_BRANCH} && \
+docker build -t ltfubuki/terraria-arm64v8 .
