a173111c87e6279deed0f3944c96cb7d8001b646
LtFubuki <99317076+LtFubuki@users.noreply.github.com>
2022-02-15 23:35:14 -0800

    Update start-terraria-docker.sh

--- start-terraria-docker.sh
+++ start-terraria-docker.sh
@@ -11,5 +11,5 @@ unzip ./${GH_REPO}-${GH_BRANCH}.zip && \
 rm ./${GH_REPO}-${GH_BRANCH}.zip && \
 cd ${GH_REPO}-${GH_BRANCH} && \
 docker build -t ltfubuki/terraria-arm64v8 . && \
-mkdir worlds && \
+if [ ! -d worlds ]; then mkdir worlds; fi && \
 docker run -itd -v worlds:/terraria/worlds -v configs:/terraria/configs -p ${SERVER_PORT}:7777/tcp --name ${SERVER_NAME}-${SERVER_COUNT} ltfubuki/terraria-arm64v8
